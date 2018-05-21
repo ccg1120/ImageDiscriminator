940fa26af094076b1e5c0cbe9168c2f3926df0d1
ccg1120 <ccg.1120@daum.net>
2018-05-21 18:25:01 +0900

    50개 불러와서 작업하는것은 완료 스트리밍기능 필요

--- Assets/Scripts/DistinctionManager.cs
+++ Assets/Scripts/DistinctionManager.cs
@@ -9,112 +9,199 @@ using TimerChecker;
 
 public class DistinctionManager : MonoBehaviour {
 
-    public int MaxLoadImageCount = 10;
+    public List<byte[]> MP4ImageByteList = new List<byte[]>(); 
+    public List<byte[]> TSImageByteList = new List<byte[]>();
 
-    public List<byte[]> Image1ByteList = new List<byte[]>(); 
-    public List<byte[]> Image2ByteList = new List<byte[]>();
-    public Texture2D[] texture2d1Array;
-    public Texture2D[] texture2d2Array;
+    public Texture2D[] MP4textureArray;
+    public Texture2D[] TStextureArray;
 
-    public string SampelPath = @"E:\Study\ImageDiscriminatorTestSample\test.jpg";
-    public string SampelPath2 = @"E:\Study\ImageDiscriminatorTestSample\test2.png";
+    public string[] MP4ImageFileNameArray;
+    public string[] TSImageFileNameArray;
 
     public string MP4Path = @"E:\Study\ImageDiscriminatorTestSample\MP4\";
     public string TSPath = @"E:\Study\ImageDiscriminatorTestSample\TS\";
 
+    private bool getFileState = false;
+
+    public enum MovieType
+    {
+        MP4,
+        TS
+    }
+
+    private readonly int MaxLoadImageCount = 50;
+    private readonly string[] ImageType = new string[] { ".jpg",".png"};
+
     // Use this for initialization
     void Start () {
         
-        //TempTexture2DCreate();
-
-        byte[] test = LoadImage(SampelPath);
-
-        Debug.Log(test.Length);
-        Debug.Log(test);
-
-        Texture2D tt = new Texture2D(1,1);
-
-        tt.LoadImage(test);
-        
-
-        Color[] colors = tt.GetPixels();
-        Debug.Log(colors.Length);
-
-        //Task task = new Task(TestTask);
-        //task.Start();
-
-        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-        sw.Reset();
-        sw.Start();
-
-        TimeChecker.StartTimer(0,"Total main");
-
-        Task<byte[]>[] TaskArray;
-
-        Task<byte[]>[] TaskArray2;
-        TaskArray = new Task<byte[]>[10];
-        TaskArray2 = new Task<byte[]>[10];
-        Debug.Log("Task Start ");
-        TimeChecker.StartTimer(1, "TaskArray");
-
-
-
-        for (int index = 0; index < TaskArray.Length; index++)
-        {
-            TaskArray[index] = new Task<byte[]>(()=> LoadImage(SampelPath));
-            TaskArray[index].Start();
-        }
-        Task.WhenAll(TaskArray);
-        for (int index = 0; index < TaskArray.Length; index++)
-        {
-            Image1ByteList.Add(TaskArray[index].Result);
-        }
-        TimeChecker.EndTimer(1);
-        TimeChecker.StartTimer(2, "TaskArray2");
-        for (int index = 0; index < TaskArray2.Length; index++)
-        {
-            TaskArray2[index] = new Task<byte[]>(() => LoadImage(SampelPath2));
-            TaskArray2[index].Start();
-        }
-        Task.WhenAll(TaskArray2);
-        for (int index = 0; index < TaskArray2.Length; index++)
-        {
-            Image2ByteList.Add(TaskArray2[index].Result);
-        }
-        TimeChecker.EndTimer(2);
-        Debug.Log("Task End ");
-        sw.Stop();
-         
-        TimeChecker.EndTimer(0);
-        Debug.Log("File Load Done!!");
-
-
+        GetALLFileName();
         TempTexture2DCreate();
-    
+
+        LoadPath2ImageByte(0);
+
+        //Task TestTask = new Task(()=> Texture2DLoadImage(0,0));
+        //TestTask.Start();
+        Texture2DLoadImage(0, MaxLoadImageCount);
+
+        
 
     }
 
     private void TempTexture2DCreate()
     {
-        Debug.Log("List Count : Image1ByteList: " + Image1ByteList.Count);
-        Debug.Log("List Count : Image2ByteList: " + Image2ByteList.Count);
+        MP4textureArray = new Texture2D[MaxLoadImageCount];
+        TStextureArray = new Texture2D[MaxLoadImageCount];
 
-        texture2d1Array = new Texture2D[MaxLoadImageCount];
-        texture2d2Array = new Texture2D[MaxLoadImageCount];
         for (int index = 0; index < MaxLoadImageCount; index++)
         {
-            texture2d1Array[index] = new Texture2D(1, 1);
-            texture2d1Array[index].LoadImage(Image1ByteList[index]);
+            MP4textureArray[index] = new Texture2D(1, 1);
+            TStextureArray[index] = new Texture2D(1, 1);
+        }
+    }
+    private void Texture2DLoadImage(int num, int lenght)
+    {
+        
 
-            texture2d2Array[index] = new Texture2D(1, 1);
-            texture2d2Array[index].LoadImage(Image2ByteList[index]);
+        Debug.Log("check1 " + MP4ImageByteList.Count);
+        Debug.Log("check2 " + TSImageByteList.Count);
+        for (int index = 0; index < lenght; index++)
+        {
+            MP4textureArray[index].LoadImage(MP4ImageByteList[index]);
+            TStextureArray[index].LoadImage(TSImageByteList[index]);
         }
     }
 
+    private void LoadPath2ImageByte(int num)
+    {
+        TimeChecker.StartTimer(0,"Load Files");
+        for (int index = 0; index < MaxLoadImageCount; index++)
+        {
+            MP4ImageByteList.Add(LoadImage(MP4Path + MP4ImageFileNameArray[index]));
+            TSImageByteList.Add(LoadImage(TSPath + TSImageFileNameArray[index]));
+        }
+        TimeChecker.EndTimer(0);
+    }
     private byte[] LoadImage(string path)
     {
         return File.ReadAllBytes(path);
     }
 
-   
+    private bool CheckImageLenght()
+    {
+        if(MP4ImageFileNameArray.Length != TSImageFileNameArray.Length)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    #region GetFilename
+    private void GetALLFileName()
+    {
+        Task<bool> T_GetMP4FileName = new Task<bool>(GetMP4FileName);
+        Task<bool> T_GetTSFileName = new Task<bool>(GetTSFileName);
+        T_GetMP4FileName.Start();
+        T_GetTSFileName.Start();
+        
+        Task.WaitAll(T_GetMP4FileName, T_GetTSFileName);
+    }
+    private void GetALLFileName(MovieType type, string path)
+    {
+        DirectoryInfo info = new DirectoryInfo(path);
+
+        if(!info.Exists)
+        {
+            info.Create();
+        }
+        int filecount = info.GetFiles().Length;
+
+        switch (type)
+        {
+            case MovieType.MP4:
+                MP4ImageFileNameArray = new string[filecount];
+                break;
+            case MovieType.TS:
+                TSImageFileNameArray = new string[filecount];
+                break;
+        }
+
+        for (int index = 0; index < info.GetFiles().Length; index++)
+        {
+            for (int index2 = 0; index2 < ImageType.Length; index2++)
+            {
+                if(info.GetFiles()[index].Name.Contains(ImageType[index2]))
+                {
+                    Debug.Log("Image type Error :" + info.GetFiles()[index].FullName);
+                    return;
+                }
+            }
+            switch (type)
+            {
+                case MovieType.MP4:
+                    MP4ImageFileNameArray[index] = info.GetFiles()[index].Name;
+                    break;
+                case MovieType.TS:
+                    TSImageFileNameArray[index] = info.GetFiles()[index].Name;
+                    break;
+            }
+        }
+    }
+    private bool GetMP4FileName()
+    {
+        DirectoryInfo info = new DirectoryInfo(MP4Path);
+        if (!info.Exists)
+        {
+            Debug.Log("GetMP4FileName Error :" + MP4Path);
+            return false;
+        }
+
+        int filecount = info.GetFiles().Length;
+        MP4ImageFileNameArray = new string[filecount];
+        FileInfo[] fileInfos = info.GetFiles();
+
+        for (int index = 0; index < filecount; index++)
+        {
+            if (fileInfos[index].Name.Contains(ImageType[0]) ||
+                fileInfos[index].Name.Contains(ImageType[0].ToUpper())||
+                fileInfos[index].Name.Contains(ImageType[1]) ||
+                fileInfos[index].Name.Contains(ImageType[1].ToUpper())
+                )
+            {
+                MP4ImageFileNameArray[index] = fileInfos[index].Name;
+            }
+        }
+        Debug.Log("MP4 File Count : " + filecount);
+        return true;
+    }
+    private bool GetTSFileName()
+    {
+        DirectoryInfo info = new DirectoryInfo(TSPath);
+        if (!info.Exists)
+        {
+            Debug.Log("GetTSFileName Error :" + TSPath);
+            return false;
+        }
+
+        int filecount = info.GetFiles().Length;
+        FileInfo[] fileInfos = info.GetFiles();
+
+        TSImageFileNameArray = new string[filecount];
+
+        for (int index = 0; index < filecount; index++)
+        {
+            if (fileInfos[index].Name.Contains(ImageType[0]) ||
+                fileInfos[index].Name.Contains(ImageType[0].ToUpper()) ||
+                fileInfos[index].Name.Contains(ImageType[1]) ||
+                fileInfos[index].Name.Contains(ImageType[1].ToUpper())
+                )
+            {
+                TSImageFileNameArray[index] = fileInfos[index].Name;
+            }
+        }
+        Debug.Log("TS File Count : " + filecount);
+        return true;
+    }
+    #endregion
+
 }
--- Assets/Scripts/ImageDistinction.cs
+++ Assets/Scripts/ImageDistinction.cs
@@ -27,15 +27,21 @@ public class ImageDistinction : MonoBehaviour {
     /// <summary>
     /// 이미지 Color[]를 이용한 판별
     /// </summary>
-    /// <param name="imagebytes1"></param>
-    /// <param name="imagebytes2"></param>
+    /// <param name="color1"></param>
+    /// <param name="color2"></param>
     /// <returns></returns>
-    public static bool ImageDistinctionFunction(Color[] imagebytes1, Color[] imagebytes2)
+    public static bool ImageDistinctionFunction(Color[] color1, Color[] color2)
     {
-        for (int index = 0; index < imagebytes1.Length; index++)
+        
+        if(color1.Length != color2.Length)
         {
-            if (imagebytes1[index] != imagebytes2[index])
+            return false;
+        }
+        for (int index = 0; index < color1.Length; index++)
+        {
+            if (color1[index] != color2[index])
             {
+                Debug.Log("Check Color1 :" + color1[index] + ", Color2  :"+ color2[index]);
                 return false;
             }
         }
