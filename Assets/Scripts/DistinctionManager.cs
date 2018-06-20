a4de7ed1b0cb7396a5ba77a0ae8981f643765f0f
ccg1120 <ccg.1120@daum.net>
2018-06-20 19:21:19 +0900

    겟픽셀에서 메모리 누수가 있는지 확인 필요, 메모리 누수가 있음

--- Assets/Scripts/DistinctionManager.cs
+++ Assets/Scripts/DistinctionManager.cs
@@ -5,88 +5,166 @@ using System.IO;
 using UnityEngine.UI;
 using System.Threading.Tasks;
 
-using TimerChecker;
+//using TimerChecker;
 
 public class DistinctionManager : MonoBehaviour {
 
-    public List<byte[]> MP4ImageByteList = new List<byte[]>(); 
-    public List<byte[]> TSImageByteList = new List<byte[]>();
+    public static  List<byte[]> MP4ImageByteList1 = new List<byte[]>(); 
+    public static List<byte[]> TSImageByteList1 = new List<byte[]>();
 
-    public Texture2D[] MP4textureArray;
-    public Texture2D[] TStextureArray;
+    public static List<byte[]> MP4ImageByteList2 = new List<byte[]>();
+    public static List<byte[]> TSImageByteList2= new List<byte[]>();
+
+    private static int currentType = 0;
+
+    public static Texture2D[] MP4textureArray;
+    public static Texture2D[] TStextureArray;
 
     public List<Color[]> MP4ColorsList = new List<Color[]>();
     public List<Color[]> TSColorsList = new List<Color[]>();
 
-    public string[] MP4ImageFileNameArray;
-    public string[] TSImageFileNameArray;
+    //public Color[][] MP4ColorsList;
+    //public Color[][] TSColorsList;
 
-    public string MP4Path = @"E:\Study\ImageDiscriminatorTestSample\MP4\";
-    public string TSPath = @"E:\Study\ImageDiscriminatorTestSample\TS\";
+    public static string[] MP4ImageFileNameArray;
+    public static string[] TSImageFileNameArray;
 
-    private int MP4ChunkCount = 0;
-    private int TSChunkCount = 0;
-
-    private int MinChunkCount = 0;
-
-    private bool getFileState = false;
-    private bool Judge = true;
+    public static  string MP4Path = @"E:\Study\ImageDiscriminatorTestSample\MP4\";
+    public static string TSPath = @"E:\Study\ImageDiscriminatorTestSample\TS\";
 
 
+    private static int MP4ChunkCount = 0;
+    private static int TSChunkCount = 0;
+            
+    private static int minFileCount = 0;
+    private static int minChunkCount = 0;
+            
+    private static int currentChunkCount = 0;
+            
+    private static int beforeChunkCount = 0;
+            
+    private static bool Judge = true;
+
+    
     public enum MovieType
     {
         MP4,
         TS
     }
 
-    private readonly int MaxLoadImageCount = 100;
-    private readonly string[] ImageType = new string[] { ".jpg",".png"};
+    private static readonly int MaxLoadImageCount = 50;
+    private static readonly int HalfLoadImageCount = 25;
+    private static readonly string[] ImageType = new string[] { ".jpg",".png"};
 
     // Use this for initialization
     void Start () {
-        
+
         GetALLFileName();
         TempTexture2DCreate();
+        int gettype = currentType;
+        Task T_LoadPath2ImageByte1 = new Task(()=> LoadPath2ImageByte(minChunkCount * MaxLoadImageCount, gettype));
+        T_LoadPath2ImageByte1.Start();
 
-        LoadPath2ImageByte(0);
+        minChunkCount++;
+        currentType = 1;
+        int gettype2 = currentType;
+        Task T_LoadPath2ImageByte2 = new Task(() => LoadPath2ImageByte(minChunkCount * MaxLoadImageCount, gettype2));
+        T_LoadPath2ImageByte2.Start();
+        currentType = 0;
 
-        //Task TestTask = new Task(()=> Texture2DLoadImage(0,0));
-        //TestTask.Start();
-        Texture2DLoadImage(0, MaxLoadImageCount);
-        Debug.Log(MP4textureArray[0].GetPixels().Length);
-        Debug.Log(TStextureArray[0].GetPixels().Length);
+        Task.WaitAll(T_LoadPath2ImageByte1, T_LoadPath2ImageByte2);
 
-        
-        TimeChecker.StartTimer(1, "Start");
-        
+        Debug.Log("Task Load Done!!");
+
+        //TimeChecker.StartTimer(1, "Start");
+
         TaskJudge();
 
 
-        TimeChecker.EndTimer(1);
+        ////TimeChecker.EndTimer(1);
 
-        Debug.Log("result : " +Judge);
+        //Debug.Log("result : " + Judge);
+
+        //MP4ImageByteList1.Clear();
+        //MP4ImageByteList2.Clear();
+        //TSImageByteList1.Clear();
+        //TSImageByteList2.Clear();
+        ColorClear();
+    }
+
+    private void Update()
+    {
+        //if(currentChunkCount > beforeChunkCount)
+        //{
+        //    beforeChunkCount = currentChunkCount;
+        //    TaskJudge();
+        //}
+    }
+
+    private void ColorClear()
+    {
+        Debug.Log("Count :" + MP4ColorsList.Count);
+        //MP4ColorsList
+        for (int index = MP4ColorsList.Count - 1; index >= 0; index--)
+        {
+            Debug.Log("index :" + index);
+            MP4ColorsList[index] = null;
+            MP4ColorsList.RemoveAt(index);
+        }
+        MP4ColorsList.Clear();
+        Debug.Log("MP4ColorsList capacity :" + MP4ColorsList.Capacity);
+
+        for (int index = TSColorsList.Count - 1; index >= 0; index--)
+        {
+            TSColorsList[index] = null;
+            TSColorsList.RemoveAt(index);
+        }
+        TSColorsList.Clear();
 
     }
 
     private void TaskJudge()
     {
-        int half =(int)( MaxLoadImageCount * 0.5f);
-        for (int index = 0; index <  MinChunkCount; index+=MaxLoadImageCount)
+        Texture2DLoadImage(currentType);
+        //Task judge1 = new Task(() => TaskImage(0));
+        //Task judge2 = new Task(() => TaskImage(HalfLoadImageCount));
+        //judge1.Start();
+        //judge2.Start();
+        //Task.WaitAll(judge1, judge2);
+
+        ColorClear();
+        Resources.UnloadUnusedAssets();
+        System.GC.Collect();
+
+        
+        //상태 교환
+        if (currentType == 0)
         {
-            Texture2DLoadImage(index, MaxLoadImageCount);
-            Task judge1 = new Task(() => TaskImage(half));
-            Task judge2 = new Task(() => TaskImage(MaxLoadImageCount));
-            judge1.Start();
-            judge2.Start();
-            Task.WaitAll(judge1,judge2);
+            currentType = 1;
+        }
+        else if (currentType == 1)
+        {
+            currentType = 0;
+        }
+        currentChunkCount++;
+
+        //DestroyTextureAll();
+    }
+
+    private static void DestroyTextureAll()
+    {
+        for (int index = MaxLoadImageCount -1 ; index >= 0; index--)
+        {
+            Destroy(MP4textureArray[index]);
+            Destroy(TStextureArray[index]);
         }
     }
 
     private void TaskImage(int num)
     {
-        for (int index = 0; index < num; index++)
+        for (int index = 0; index < HalfLoadImageCount; index++)
         {
-            if(!ImageDistinction.ImageDistinctionFunction(MP4ColorsList[index], TSColorsList[index]))
+            if(!ImageDistinction.ImageDistinctionFunction(MP4ColorsList[num + index], TSColorsList[index]))
             {
                 Debug.LogError("Image X");
                 return;
@@ -105,34 +183,129 @@ public class DistinctionManager : MonoBehaviour {
             MP4textureArray[index] = new Texture2D(1, 1);
             TStextureArray[index] = new Texture2D(1, 1);
         }
-    }
-    private void Texture2DLoadImage(int num, int lenght)
-    {
-        Debug.Log("check1 " + MP4ImageByteList.Count);
-        Debug.Log("check2 " + TSImageByteList.Count);
-        MP4ColorsList.Clear();
-        TSColorsList.Clear();
-        for (int index = 0; index < lenght; index++)
-        {
-            MP4textureArray[index].LoadImage(MP4ImageByteList[index]);
-            MP4ColorsList.Add(MP4textureArray[index].GetPixels());
-            TStextureArray[index].LoadImage(TSImageByteList[index]);
-            TSColorsList.Add(TStextureArray[index].GetPixels());
-        }
-        
+
+        //MP4ColorsList = new Color[MaxLoadImageCount][];
+        //TSColorsList = new Color[MaxLoadImageCount][];
     }
 
-    private void LoadPath2ImageByte(int num)
+    private void Texture2DLoadImage(int type)
     {
-        TimeChecker.StartTimer(0,"Load Files");
+        Debug.Log("check1 " + MP4ImageByteList1.Count);
+        Debug.Log("check2 " + TSImageByteList1.Count);
+
+        Debug.Log("type : "+type);
+        ColorClear();
         for (int index = 0; index < MaxLoadImageCount; index++)
         {
-            MP4ImageByteList.Add(LoadImage(MP4Path + MP4ImageFileNameArray[index]));
-            TSImageByteList.Add(LoadImage(TSPath + TSImageFileNameArray[index]));
+            switch(type)
+            {
+                case 0:
+                    MP4textureArray[index].LoadImage(MP4ImageByteList1[index]);
+                    TStextureArray[index].LoadImage(TSImageByteList1[index]);
+                    break;
+
+                case 1:
+                    MP4textureArray[index].LoadImage(MP4ImageByteList2[index]);
+                    TStextureArray[index].LoadImage(TSImageByteList2[index]);
+                    break;
+            }
+            Color[] mp4color = MP4textureArray[index].GetPixels();
+            
+            //MP4ColorsList[index] = new Color[mp4color.Length];
+            //MP4ColorsList[index] = mp4color;
+
+            MP4ColorsList.Add(mp4color);
+            Color[] tscolor = TStextureArray[index].GetPixels();
+            
+
+            //TSColorsList[index] = new Color[mp4color.Length];
+            //TSColorsList[index] = tscolor;
+            TSColorsList.Add(tscolor);
+
+            Destroy(MP4textureArray[index]);
+            Destroy(TStextureArray[index]);
+
+            MP4textureArray[index] = null;
+            TStextureArray[index] = null;
         }
-        TimeChecker.EndTimer(0);
+        //DestroyTextureAll();
+
     }
-    private byte[] LoadImage(string path)
+
+    private static void LoadPath2ImageByte(int num, int type)
+    {
+        Debug.Log("LoadPath2ImageByte : type :: " + type);
+        int intype = type;
+        Debug.Log("LoadPath2ImageByte : intype :: " + intype);
+
+        switch (type)
+        {
+            case 0:
+                MP4ImageByteList1.Clear();
+                TSImageByteList1.Clear();
+                break;
+            case 1:
+                MP4ImageByteList2.Clear();
+                TSImageByteList2.Clear();
+                break;
+        }
+
+
+        for (int index = 0; index < MaxLoadImageCount; index++)
+        {
+            switch (type)
+            {
+                case 0:
+                    MP4ImageByteList1.Add(LoadImage(MP4Path + MP4ImageFileNameArray[num + index]));
+                    TSImageByteList1.Add(LoadImage(TSPath + TSImageFileNameArray[num + index]));
+                    break;
+                case 1:
+                    MP4ImageByteList2.Add(LoadImage(MP4Path + MP4ImageFileNameArray[num + index]));
+                    TSImageByteList2.Add(LoadImage(TSPath + TSImageFileNameArray[num + index]));
+                    break;
+            }
+        }
+
+        return;
+        //if (currentType == 0)
+        //{
+        //    currentType = 1;
+        //}
+        //else if(currentType == 1)
+        //{
+        //    currentType = 0;
+        //}
+        //minChunkCount++;
+    }
+
+    private void LoadPath2ImageByte()
+    {
+        //TimeChecker.StartTimer(0, "Load Files");
+
+        int num = currentChunkCount * MaxLoadImageCount;
+
+        for (int index = 0; index < MaxLoadImageCount; index++)
+        {
+            switch (currentType)
+            {
+                case 0:
+                    MP4ImageByteList1.Add(LoadImage(MP4Path + MP4ImageFileNameArray[num + index]));
+                    TSImageByteList1.Add(LoadImage(TSPath + TSImageFileNameArray[num + index]));
+                    break;
+                case 1:
+                    MP4ImageByteList2.Add(LoadImage(MP4Path + MP4ImageFileNameArray[num + index]));
+                    TSImageByteList2.Add(LoadImage(TSPath + TSImageFileNameArray[num + index]));
+                    break;
+            }
+        }
+
+        currentChunkCount++;
+
+        //TimeChecker.EndTimer(0);
+    }
+
+
+    private static byte[] LoadImage(string path)
     {
         return File.ReadAllBytes(path);
     }
@@ -147,7 +320,7 @@ public class DistinctionManager : MonoBehaviour {
     }
 
     #region GetFilename
-    private void GetALLFileName()
+    private static void GetALLFileName()
     {
         Task<bool> T_GetMP4FileName = new Task<bool>(GetMP4FileName);
         Task<bool> T_GetTSFileName = new Task<bool>(GetTSFileName);
@@ -158,15 +331,17 @@ public class DistinctionManager : MonoBehaviour {
 
         if(MP4ChunkCount > TSChunkCount)
         {
-            MinChunkCount = TSChunkCount;
+            minFileCount = TSChunkCount;
         }
         else
         {
-            MinChunkCount = MP4ChunkCount;
+            minFileCount = MP4ChunkCount;
         }
-        Debug.Log("Minchungk : " + MinChunkCount);
+        Debug.Log("Minchungk : " + minFileCount);
+
+        minChunkCount = (int)(minFileCount / MaxLoadImageCount);
     }
-    private void GetALLFileName(MovieType type, string path)
+    private static  void GetALLFileName(MovieType type, string path)
     {
         DirectoryInfo info = new DirectoryInfo(path);
 
@@ -207,7 +382,7 @@ public class DistinctionManager : MonoBehaviour {
             }
         }
     }
-    private bool GetMP4FileName()
+    private static bool GetMP4FileName()
     {
         DirectoryInfo info = new DirectoryInfo(MP4Path);
         if (!info.Exists)
@@ -237,7 +412,7 @@ public class DistinctionManager : MonoBehaviour {
         Debug.Log("MP4 File Count : " + filecount);
         return true;
     }
-    private bool GetTSFileName()
+    private static bool GetTSFileName()
     {
         DirectoryInfo info = new DirectoryInfo(TSPath);
         if (!info.Exists)
